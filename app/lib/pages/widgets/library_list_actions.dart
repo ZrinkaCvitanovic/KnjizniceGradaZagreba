87a041194be4a3071fc6fd9780192edfda86ea91
Zrinka <zrinka.cvitanovic@estudent.hr>
2024-12-31 18:51:12 +0100

    changed docker-compose

--- app/lib/pages/widgets/library_list_actions.dart
+++ app/lib/pages/widgets/library_list_actions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:web_library_fe/controller/library_notifier.dart';
 import 'package:web_library_fe/di.dart';
-import 'package:web_library_fe/service/export_service.dart';
+//import 'package:web_library_fe/service/export_service.dart';
 import 'dart:html' as html;
 
 class LibraryListActions extends ConsumerStatefulWidget {
